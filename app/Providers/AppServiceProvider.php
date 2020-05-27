887c1112919587b766de4fa2e3b6d51554e54d31
yosh1ba <hk.r8.718@gmail.com>
2020-05-28 05:47:41 +0900

    [Fix] AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);   //追加
-        if (App::environment('production')) {
+        if (env('APP_ENV') === 'production')
+        {
             URL::forceScheme('https');
         }
     }
